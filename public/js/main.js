b90804c56fe69b7e6d3e979490df560d06b7f131
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-01-24 23:16:03 +0100

    optimization

--- public/js/main.js
+++ public/js/main.js
@@ -18,34 +18,15 @@ console.log(matches);
 cards.forEach((element, index) => {
     element.dataset.card = values[index];
     
-   
-    // switch (values[index]) {
-    //     case "1":
-    //         element.style.backgroundImage = "url(../public/img/bbe6f4103563031.5f50b135a0b96.jpg)";
-    //         break;
-    //     case "2":
-    //         element.style.backgroundImage = "url(../public/img/2.jpg)";
-    //         break;
-    //     case "3":
-    //         element.style.backgroundImage = "url(../public/img/3.jpg)";
-    //         break;
-    //     case "4":
-    //         element.style.backgroundImage = "url(../public/img/4.jpg)";
-    //         break;
-        
-    //     default:
-            
-    //         element.style.backgroundImage = "url('defaultURL')";
-    //         break;
-    // }
+
 });
 
 
-    cards.forEach(element => {
-        element.addEventListener("click", () => {
-        element.classList.toggle("flipped");
-        element.classList.toggle("back");
-        element.classList.toggle("front");
+cards.forEach(element => {
+    element.addEventListener("click", () => {
+        element.classList.add("flipped");
+        element.classList.remove("back");
+        element.classList.add("front");
         cardArray.push(element)
         console.log(cardArray);
         if (cardArray.length == 2){
@@ -57,23 +38,19 @@ cards.forEach((element, index) => {
             }
             if (matches == cards.length / 2){
                 console.log("Congratulations! You won!");
-            }else {
+            } else {
                 setTimeout(() => {
-                    
                     cardArray[0].classList.remove("flipped");
+                    cardArray[0].classList.remove("front");
+                    cardArray[0].classList.add("back");
                     cardArray[1].classList.remove("flipped");
+                    cardArray[1].classList.remove("front");
+                    cardArray[1].classList.add("back");
                     cardArray = [];
-                                
+
                 }, 2000);
-                
             }
-            
-
         }
-        
-        
-        });
-
     });
-
+});
 
