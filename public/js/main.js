c3e4acb1ed2f073369d673a39bac245f6796d8af
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-01-24 21:26:51 +0100

    changes everywhere  and optimization

--- public/js/main.js
+++ public/js/main.js
@@ -1,46 +1,79 @@
 let cards = document.querySelectorAll(".card")
 let cardArray = [];
 let matches = 0;
+let values = ["1", "2", "3", "4", "1", "2", "3", "4"];
+
+ 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+shuffleArray(values);
+
 console.log(matches);
 
-
-cards.forEach(element => {
-    element.addEventListener("click", () => {
-    element.classList.toggle("flipped");
-    cardArray.push(element)
-    console.log(cardArray);
-    if (cardArray.length == 2){
-        if (cardArray[0].dataset.card === cardArray[1].dataset.card){
-            matches++;
-            cardArray = []
-            console.log(matches);
-            console.log(cardArray);
-        }
-        if (matches == 4){
-            console.log("Congratulations!");
-        }else {
-            setTimeout(() => {
-                
-                cardArray[0].classList.remove("flipped");
-                cardArray[1].classList.remove("flipped");
-                cardArray = [];
-                             
-            }, 2000);
-            
-        }
+cards.forEach((element, index) => {
+    element.dataset.card = values[index];
+    
+   
+    switch (values[index]) {
+        // case "1":
+        //     element.style.backgroundImage = "url(../public/img/bbe6f4103563031.5f50b135a0b96.jpg)";
+        //     break;
+        case "2":
+            element.style.backgroundImage = "url(../public/img/2.jpg)";
+            break;
+        case "3":
+            element.style.backgroundImage = "url(../public/img/3.jpg)";
+            break;
+        case "4":
+            element.style.backgroundImage = "url(../public/img/4.jpg)";
+            break;
         
-
+        default:
+            
+            element.style.backgroundImage = "url('defaultURL')";
+            break;
     }
-    
-    
-    });
-
 });
 
 
-// if the flipped class is .flipped#first1
-// let test = document.querySelector("#first1")
-// test.addEventListener("click", () => {
-//     test.classList.toggle('flipped');
-// })
+    cards.forEach(element => {
+        element.addEventListener("click", () => {
+        element.classList.toggle("flipped");
+        element.classList.remove("back");
+        element.classList.add("front");
+        cardArray.push(element)
+        console.log(cardArray);
+        if (cardArray.length == 2){
+            if (cardArray[0].dataset.card === cardArray[1].dataset.card){
+                matches++;
+                cardArray = []
+                console.log(matches);
+                console.log(cardArray);
+            }
+            if (matches == cards.length / 2){
+                console.log("Congratulations! You won!");
+            }else {
+                setTimeout(() => {
+                    
+                    cardArray[0].classList.remove("flipped");
+                    cardArray[1].classList.remove("flipped");
+                    cardArray = [];
+                                
+                }, 2000);
+                
+            }
+            
+
+        }
+        
+        
+        });
+
+    });
+
 
